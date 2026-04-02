495235104313ae660e746f5844aea745ef897eb8
toasterw <71234319+toasterw@users.noreply.github.com>
2026-04-02 16:01:06 +1300

    init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
+
+const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.login(process.env.DISCORD_TOKEN);
